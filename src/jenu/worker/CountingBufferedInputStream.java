ba420dd5d88e2fbbe88093d22b8287dc629dda32
maazl <github@maazl.de>
2018-10-20 11:40:59 +0200

    Fix: do not count bytes twice

--- src/jenu/worker/CountingBufferedInputStream.java
+++ src/jenu/worker/CountingBufferedInputStream.java
@@ -19,7 +19,7 @@ final class CountingBufferedInputStream extends BufferedInputStream
 		super(in, size);
 	}
 
-	public int read() throws IOException
+	public synchronized int read() throws IOException
 	{
 		int c = super.read();
 		if (c != -1)
@@ -30,24 +30,12 @@ final class CountingBufferedInputStream extends BufferedInputStream
 		return c;
 	}
 
-	public int read(byte[] b, int off, int len) throws IOException
+	public synchronized int read(byte[] b, int off, int len) throws IOException
 	{
 		int count = super.read(b, off, len);
 		if (count != -1)
 		{	bytesRead += count;
 			for (int i = off; i < off + len; ++i)
-				if (b[i] == '\n')
-					++line;
-		}
-		return count;
-	}
-
-	public int read(byte[] b) throws IOException
-	{
-		int count = super.read(b);
-		if (count != -1)
-		{	bytesRead += count;
-			for (int i = 0; i < b.length; ++i)
 				if (b[i] == '\n')
 					++line;
 		}
